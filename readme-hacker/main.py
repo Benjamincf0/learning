31409d83e3427678212cc78c00926b8489e4c47c
Benjamin Curis-Friedman <benjamincf01@gmail.com>
2026-04-12 18:48:10 -0400

    lol making me get more views for fun heeheheh

---
+++ readme-hacker/main.py
@@ -0,0 +1,28 @@
+import time
+
+from playwright.sync_api import sync_playwright
+from tqdm import tqdm
+
+
+def open_readme():
+    playwright = sync_playwright().start()
+
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://github.com/Benjamincf0")
+    time.sleep(1)
+    for _ in tqdm(range(1000)):
+        page.reload()
+        time.sleep(0.2)
+    browser.close()
+
+    playwright.stop()
+
+
+def main():
+    print("Hello from readme-hacker!")
+    open_readme()
+
+
+if __name__ == "__main__":
+    main()
