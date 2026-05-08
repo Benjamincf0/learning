5fd45220dcf8e6ed4b4c8c90300b0aa1226ef545
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-05-08 00:20:54 -0400

    poop

--- robotViewer/src/App.tsx
+++ robotViewer/src/App.tsx
@@ -2,25 +2,36 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera, CameraControls, Environment } from '@react-three/drei'
-import { useLoader } from '@react-three/fiber'
-import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  PerspectiveCamera,
+  CameraControls,
+  FaceLandmarker,
+  Environment,
+  FaceControls,
+} from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader.js";
 // import './App.css'
-function RobotPartMesh({ position }) {
+function RobotPartMesh() {
   // Load the DAE file from the public folder
-  const collada = useLoader(ColladaLoader, 'meca_500_r3_j3.dae')
-  const mesh = collada.scene.children.find((child) => child.isMesh)
+  const collada = useLoader(ColladaLoader, "meca_500_r3_j3.dae");
+  const mesh = collada.scene.children.find((child) => child.isMesh);
 
   if (!mesh) return null;
 
   // Render the scene contained within the Collada object
   return (
-    <mesh position={position} scale={9} geometry={mesh.geometry}>
-      <meshStandardMaterial wireframe color="hotpink" metalness={0.8} roughness={0.5} />
+    <mesh scale={9} geometry={mesh.geometry}>
+      <meshStandardMaterial
+        wireframe
+        color="hotpink"
+        metalness={0.8}
+        roughness={0.5}
+      />
     </mesh>
-  )
+  );
   // return (
   //   <mesh>
   //     <boxGeometry args={[3, 3, 3]} />
@@ -30,28 +41,35 @@ function RobotPartMesh({ position }) {
 }
 
 function App() {
-
   return (
     <>
-      <div style={{ height: '100%', width: '100%', position: 'relative', display: 'flex', flexDirection: 'column' }}>
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         <h1>Robot Viewer</h1>
-        <Canvas >
-          <PerspectiveCamera makeDefault position={[0, 3, 10]} />
-          <CameraControls />
-          <ambientLight intensity={1.2} />
-          <directionalLight intensity={0.9} position={[1, 3, 5]} />
-          <directionalLight intensity={0.9} position={[-1, -3, -5]} />
-          <Suspense fallback={null}>
-            <RobotPartMesh position={[1, 1, 1]} />
-          </Suspense>
-          <Environment preset={"park"} background={true} />
-        </Canvas>
+        <FaceLandmarker>
+          <Canvas>
+            {/* <PerspectiveCamera makeDefault position={[0, 3, 10]} /> */}
+            {/* <CameraControls /> */}
+            <ambientLight intensity={1.2} />
+            <directionalLight intensity={0.9} position={[1, 3, 5]} />
+            <directionalLight intensity={0.9} position={[-1, -3, -5]} />
+            <Suspense fallback={null}>
+              <FaceControls />
+              <RobotPartMesh />
+            </Suspense>
+            {/* <Environment preset={"city"} background={true} /> */}
+          </Canvas>
+        </FaceLandmarker>
       </div>
     </>
-  )
+  );
 }
 
-
-
-
-export default App
+export default App;
