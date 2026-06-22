a64916940a07bd0cbc61423277706e04630a919e
Benjamin <bencf99@gmail.com>
2026-06-22 03:54:08 -0400

    demo script

--- asyncpy/main.py
+++ asyncpy/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from asyncpy!")
+import myasyncpy
+import myasyncpy.common
 
+@myasyncpy.common.Coroutine
+def processA():
+    print(f"Begin A")
+    myasyncpy.sleep(1)
+    print(f"End A")
+
+@myasyncpy.common.Coroutine
+def processB():
+    print(f"Begin B")
+    myasyncpy.sleep(1)
+    print(f"End B")
+
+@myasyncpy.common.Coroutine
+def processC():
+    print(f"Begin C")
+    myasyncpy.sleep(1)
+    print(f"End C")
 
 if __name__ == "__main__":
-    main()
+    print("HEY WORLD")
+    myasyncpy.create_task(processA)
+    myasyncpy.create_task(processB)
+    myasyncpy.create_task(processC)
+    myasyncpy.run_loop()
