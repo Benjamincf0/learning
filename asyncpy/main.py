b4326b1db0da2b91971f5a06b47eb43e6209063d
Benjamin <bencf99@gmail.com>
2026-06-25 15:23:55 -0400

    my async lib works :)))

--- asyncpy/main.py
+++ asyncpy/main.py
@@ -4,19 +4,19 @@ import myasyncpy.common
 @myasyncpy.common.Coroutine
 def processA():
     print(f"Begin A")
-    myasyncpy.sleep(1)
+    yield myasyncpy.sleep(1)
     print(f"End A")
 
 @myasyncpy.common.Coroutine
 def processB():
     print(f"Begin B")
-    myasyncpy.sleep(1)
+    yield myasyncpy.sleep(1)
     print(f"End B")
 
 @myasyncpy.common.Coroutine
 def processC():
     print(f"Begin C")
-    myasyncpy.sleep(1)
+    yield myasyncpy.sleep(2)
     print(f"End C")
 
 if __name__ == "__main__":
