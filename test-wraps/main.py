c70d8186937b0839a2e912558788c9e5978cf6b8
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-09-03 16:09:47 -0400

    stuff

--- test-wraps/main.py
+++ test-wraps/main.py
@@ -11,15 +11,23 @@ class MyClass2:
     @wraps(MyClass.doSomething)
     def doSomethingWrapper(self, *args, **kwargs):
         """ Wrapper docstring """
-        ...
+        return myObj.doSomething(*args, **kwargs)
+
+@wraps(MyClass.doSomething)
+def doSomethingWrapper2(*args, **kwargs):
+    """ Wrapper docstring """
+    return myObj.doSomething(*args, **kwargs)
+
 
 myObj2 = MyClass2()
 
 def main():
-    myObj2.doSomethingWrapper(3, "poo")
-    print(myObj2.doSomethingWrapper.__name__)
-    print(myObj.doSomething.__annotations__, myObj2.doSomethingWrapper.__annotations__)
-    print(myObj2.doSomethingWrapper.__doc__)
+    myObj2.doSomethingWrapper(myObj2, 'b')
+    doSomethingWrapper2('c', 'd')
+    print(MyClass.doSomething.__annotations__)
+    print(myObj.doSomething.__annotations__)
+    print(myObj2.doSomethingWrapper.__annotations__)
+    print(doSomethingWrapper2.__annotations__)
 
 
 if __name__ == "__main__":
