c63ab7df3f7c06656ed21f94d45390bf539dc129
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-09-10 16:45:07 -0400

    pattern matching

--- COMP302/hello/bin/main.ml
+++ COMP302/hello/bin/main.ml
@@ -37,4 +37,64 @@ let me : person = (my_name, my_height)
 
 let is_tall (p : person) : bool = snd p > 190
 
-let is_tall ( (name, height) : person ) : bool = height > 190
+let is_tall ( (name, height) : person ) : bool = height > 191
+
+
+(* ENUMERATED TYPES *)
+type hand = Rock | Paper | Scissors
+
+let my_hand = Rock
+let your_hand = Paper
+
+let beats (h1 : hand) (h2 : hand) : bool =
+    match h1 with
+    | Rock -> h2 = Scissors
+                (* In this case, h1 (i.e. Rock) beats h2 when h2 is Scissors. *)
+    | Paper -> h2 = Rock
+    | Scissors -> h2 = Paper ;;
+
+    (* by returining a bool, we can't express all three possible outcomes.
+       There's a third outcome: a draw. *)
+
+type outcome = Win | Lose | Draw
+
+let play h1 h2 : outcome =
+    match h1 with
+        | Rock -> 
+            (match h2 with
+            | Rock -> Draw
+            | Paper -> Lose
+            | Scissors -> Win)
+        | Paper ->
+            begin match h2 with
+                | Rock -> Win
+                | Paper -> Draw
+                | Scissors -> Lose
+            end
+        | Scissors ->
+            begin match h2 with
+                | Rock -> Lose
+                | Paper -> Win
+                | Scissors -> Draw
+            end
+
+let play h1 h2 : outcome =
+    if h1 = h2 then Draw
+    else if beats h1 h2 then Win
+    else Lose
+
+let play h1 h2 : outcome =
+    if h1 = h2 then Draw else
+        match (h1, h2) with
+        | (Rock, Paper) -> Lose
+        | (Paper, Scissors) -> Lose
+        | (Scissors, Rock) -> Lose
+        | _ -> Win
+
+let play h1 h2 : outcome =
+    match (h1, h2) with
+    | (me, you) when me = you -> Draw
+    | (Rock, Paper) -> Lose
+    | (Paper, Scissors) -> Lose
+    | (Scissors, Rock) -> Lose
+    | _ -> Win
